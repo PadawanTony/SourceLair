8cb94bdf3040c3569b34b645aa690e2c3451714a
phantomDuck <anthonykalogeropoulos@gmail.com>
2015-06-01 15:44:53 +0000

    Create index file and use Monolog

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require 'vendor/autoload.php';
+date_default_timezone_set('Europe/Athens');
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+
+$log->addWarning('Foo');
+
+echo "Hello World!";
+
+
+?>
